cf82ac29aa7b9de1b227d62636a97176c8357a01
Rony <ronyjavierramirezalvarado@gmail.com>
2024-04-02 08:01:21 -0600

    Configuere js

--- Js/Menu.js
+++ Js/Menu.js
@@ -10,11 +10,13 @@ function Menus(){
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ms-auto py-4 py-lg-0">
                 <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="index.html">Inicio</a></li>
+                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="Catalogo.html">Catalogo</a></li>
                 <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="about.html">Sobre nosotros</a>
                 </li>
                 <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="services.html">Alquiler de
                         Servicios</a></li>
                 <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="contact.html">Contactanos</a></li>
+                
             </ul>
         </div>
     </div>
